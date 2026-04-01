3ce35913a335977b06c7ce88a05f221a1ac63f46
Jisnu Hazra <jisnuhazra@gmail.com>
2026-04-01 16:09:08 +0530

    fix wrong settings module path

--- crop_recommend/asgi.py
+++ crop_recommend/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crop_recommend.crop_recommend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crop_recommend.settings')
 
 application = get_asgi_application()
--- crop_recommend/wsgi.py
+++ crop_recommend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crop_recommend.crop_recommend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crop_recommend.settings')
 
 application = get_wsgi_application()
