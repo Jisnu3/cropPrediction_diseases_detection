e4f8103ee45e7c8bdc7c7e644f62842491ffa0cd
Jisnu Hazra <jisnuhazra@gmail.com>
2026-06-09 22:44:24 +0530

    Fix static files path

--- crop_recommend/settings.py
+++ crop_recommend/settings.py
@@ -115,9 +115,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+# Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "recommender" / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 LOGIN_URL = '/login/'
