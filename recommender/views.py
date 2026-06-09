028ded17aafface6cfe584254639e439b9586b1b
Jisnu Hazra <jisnuhazra@gmail.com>
2026-06-09 09:14:21 +0530

    Remove global tensorflow import

--- recommender/views.py
+++ recommender/views.py
@@ -332,7 +332,6 @@ def admin_change_password_view(request):
 
 
 from pathlib import Path
-import tensorflow as tf
 import numpy as np
 from PIL import Image
 import base64 
@@ -343,11 +342,15 @@ model = None
 
 def get_model():
     global model
+
     if model is None:
+        import tensorflow as tf
+
         model = tf.keras.models.load_model(
-            BASE_DIR/"MachineLearning"/"trained_model.keras",
+            BASE_DIR / "MachineLearning" / "trained_model.keras",
             compile=False
         )
+
     return model
 
 class_names = [
@@ -519,6 +522,7 @@ def disease_detection_view(request):
                 img = img.resize((128, 128))
 
                 # IMPORTANT: NO NORMALIZATION (matches training)
+                import tensorflow as tf
                 img_array = tf.keras.preprocessing.image.img_to_array(img)
                 img_array = img_array.astype("float32")
                 img_array = np.expand_dims(img_array, axis=0)
